21b01f6b530bc697c4b2b85018ea033f763f73e9
Hitalo Loose <hitaloose@gmail.com>
2021-11-29 08:23:35 -0300

    persist log

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,73 @@
 import express from "express";
+import { S3 } from "aws-sdk";
+
+const credentials = {
+  id: "AKIAWUX6FASKVO7PNKNZ",
+  private: "Ozvi9iY7dnoF/iKK38HK+0vbanZ0IGleVKXn9Plq",
+};
+
+const s3 = new S3({
+  region: "us-east-2",
+  apiVersion: "2006-03-01",
+  credentials: {
+    accessKeyId: credentials.id,
+    secretAccessKey: credentials.private,
+  },
+});
+
+const BUCKET = "fojat96378";
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/handler", (req, res) => {
-  console.log("@HANDLER");
-  console.log(req.body);
-
+app.post("/handler", async (req, res) => {
   const data = JSON.parse(
     Buffer.from(req.body.message?.data, "base64").toString("utf-8")
   );
+
+  const jsonLog: any = {};
+  jsonLog.type = "@HANDLER";
+  jsonLog.body = req.body;
+  jsonLog.data = data;
+
+  const params: S3.PutObjectRequest = {
+    Bucket: BUCKET,
+    Body: JSON.stringify(jsonLog, null, 2),
+    Key: `log/${new Date().getTime()}.json`,
+    ACL: "public-read",
+  };
+
+  await s3.upload(params).promise();
 
   if (data.error) {
     throw new Error();
   }
 
-  console.log(data);
-
+  console.log("Handler");
   res.send();
 });
 
-app.post("/deadletter", (req, res) => {
-  console.log("@DEAD_LETTER");
-  console.log(req.body);
-
+app.post("/deadletter", async (req, res) => {
   const data = JSON.parse(
     Buffer.from(req.body.message?.data, "base64").toString("utf-8")
   );
 
-  console.log(data);
+  const jsonLog: any = {};
+  jsonLog.type = "@DEAD_LETTER";
+  jsonLog.body = req.body;
+  jsonLog.data = data;
 
+  const params: S3.PutObjectRequest = {
+    Bucket: BUCKET,
+    Body: JSON.stringify(jsonLog, null, 2),
+    Key: `log/${new Date().getTime()}.json`,
+    ACL: "public-read",
+  };
+
+  await s3.upload(params).promise();
+
+  console.log("Deadletter");
   res.send();
 });
 
