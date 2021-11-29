4e19aaff4a25640c08897f94ea00d108602b2bcb
Hitalo Loose <hitaloose@gmail.com>
2021-11-29 07:27:01 -0300

    fix

--- src/app.ts
+++ src/app.ts
@@ -5,21 +5,31 @@ const app = express();
 app.use(express.json());
 
 app.post("/handler", (req, res) => {
+  console.log("@HANDLER");
+  console.log(req.body);
+
   const data = JSON.parse(
     Buffer.from(req.body.message?.data, "base64").toString("utf-8")
   );
-
-  console.log(data);
 
   if (data.error) {
     throw new Error();
   }
 
+  console.log(data);
+
   res.send();
 });
 
 app.post("/deadletter", (req, res) => {
+  console.log("@DEAD_LETTER");
   console.log(req.body);
+
+  const data = JSON.parse(
+    Buffer.from(req.body.message?.data, "base64").toString("utf-8")
+  );
+
+  console.log(data);
 
   res.send();
 });
