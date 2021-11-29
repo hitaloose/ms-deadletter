450535656f6f67339b7f2a8052588b475cdbc4c4
Hitalo Loose <hitaloose@gmail.com>
2021-11-29 06:11:02 -0300

    init

---
+++ src/main/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/handler", (req, res) => {
+  const data = JSON.parse(
+    Buffer.from(req.body.message?.data, "base64").toString("utf-8")
+  );
+
+  if (data.error) {
+    throw new Error();
+  }
+
+  res.send();
+});
+
+app.post("/deadletter", (req, res) => {
+  console.log(req.body);
+
+  res.send();
+});
+
+export { app };
